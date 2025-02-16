6bf034632f3c117da944fbb2b182095842705a4c
jaehwan02 <jhrick0129@gmail.com>
2025-02-17 00:49:42 +0900

    chore : 테이블 생성 오류 수정

--- app/models/file.py
+++ app/models/file.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, func
+from sqlalchemy import Column, Integer, String, Text, DateTime, func
 from app.database import Base
 
 class File(Base):
@@ -6,7 +6,7 @@ class File(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String(255), nullable=False)
-    content = Column(String, nullable=False)
+    content = Column(Text, nullable=False)
     created_at = Column(DateTime, nullable=False, server_default=func.now())
     updated_at = Column(DateTime, nullable=False, server_default=func.now(), onupdate=func.now())
     hashed_id = Column(String(255), unique=True, index=True, nullable=True)
